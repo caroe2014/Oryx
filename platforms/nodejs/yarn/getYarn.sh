3147d9c98dffe4804346ec7ddeca89581422bebb
Kiran <kichalla@microsoft.com>
2020-08-21 09:51:29 -0700

    fixed yarn url

--- platforms/nodejs/yarn/getYarn.sh
+++ platforms/nodejs/yarn/getYarn.sh
@@ -3,8 +3,9 @@ set -ex
 
 YARN_VERSION="$1"
 
-curl -fsSLO --compressed "https://yarnpkg.com/downloads/$YARN_VERSION/yarn-v$YARN_VERSION.tar.gz"
-curl -fsSLO --compressed "https://yarnpkg.com/downloads/$YARN_VERSION/yarn-v$YARN_VERSION.tar.gz.asc"
+baseUrl="https://github.com/yarnpkg/yarn/releases/download"
+curl -fsSLO --compressed "$baseUrl/v$YARN_VERSION/yarn-v$YARN_VERSION.tar.gz"
+curl -fsSLO --compressed "$baseUrl/v$YARN_VERSION/yarn-v$YARN_VERSION.tar.gz.asc"
 gpg --batch --verify yarn-v$YARN_VERSION.tar.gz.asc yarn-v$YARN_VERSION.tar.gz
 targetDir="/tmp/compressedSdk/yarn"
 mkdir -p "$targetDir"
